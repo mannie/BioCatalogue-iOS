bb1d78a0dc47bada81de663ea540b2407feee26f
Mannie Tagarira <mannie@dimensionsunmarked.com>
2011-05-02 15:51:41 +0100

    Updated copyright notice.

--- Shared/Controllers/SearchViewController.h
+++ Shared/Controllers/SearchViewController.h
@@ -3,7 +3,7 @@
 //  BioMonitor
 //
 //  Created by Mannie Tagarira on 01/02/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 myGrid (University of Manchester). All rights reserved.
 //
 
 @class DetailViewController_iPad, UserDetailViewController_iPhone;
